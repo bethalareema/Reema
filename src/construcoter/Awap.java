15d3458e23c127443b3c6accfc3a21fc2610355a
admin <admin@REEMA>
2026-07-28 10:38:11 +0530

    Swapping program

---
+++ src/construcoter/Awap.java
@@ -0,0 +1,25 @@
+package construcoter;
+
+public class Awap {
+	
+	
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		
+		
+		System.out.println("Before Swaping a:"+b);
+		System.out.println("after Swaping b:"+a);
+		System.out.println("............................");
+		System.out.println("Before Swaping a:"+a);
+		System.out.println("after Swaping b:"+b);
+		
+
+	}
+
+}
