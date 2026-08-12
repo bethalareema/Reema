7c8e133dbf3918804b56981959c1828a4dc454f2
admin <admin@REEMA>
2026-08-12 12:29:49 +0530

    prime number

---
+++ src/com/poeratorsprograms/selecationstaements/PrimeNumaber.java
@@ -0,0 +1,38 @@
+package com.poeratorsprograms.selecationstaements;
+
+import java.util.Scanner;
+
+public class PrimeNumaber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Prime Number :");
+		int n=sc.nextInt();
+		
+		for(int i=0;i<=n;i++) {
+		if(isprime(i)) {
+			System.out.println(i);
+			
+		}
+	  }
+	}
+	static boolean isprime(int n) {
+		boolean prime=true;
+			if(n==0||n==1) {
+				return false;
+				
+			}
+			for(int i =2;i<=n/2;i++) {
+				if(n%i==0) {
+					prime=false;
+					break;
+				}
+			}
+			
+		
+		return prime;
+		
+	}
+			
+		
+}
