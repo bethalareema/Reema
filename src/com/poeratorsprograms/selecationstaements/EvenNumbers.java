939c3658775338a9e6bdddf2ac41d6e1f0ab998c
admin <admin@192.168.0.102>
2026-08-13 22:40:45 +0530

    even numbers and factors

---
+++ src/com/poeratorsprograms/selecationstaements/EvenNumbers.java
@@ -0,0 +1,21 @@
+package com.poeratorsprograms.selecationstaements;
+
+import java.util.Scanner;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		int n=sc.nextInt();
+		int sum=0;
+
+		for(int i=0;i<=n;i++) {
+			
+			sum=i+sum;
+		
+		}
+		System.out.println(sum);
+	}
+
+}
--- src/com/poeratorsprograms/selecationstaements/Factors.java
+++ src/com/poeratorsprograms/selecationstaements/Factors.java
@@ -1,16 +1,23 @@
 package com.poeratorsprograms.selecationstaements;
 
 import java.util.Scanner;
+
+//Factors of number are whole number
+//that didide the number exactly without leaving a remainder.
+
+//wap to print factor or divisor of a given number ..?
+//input number---6
+//output number ----- 2 3 1 =6 perfect number,28
 public class Factors {
 	void factorinfo(int n) {
 		for(int i=1;i<=n/2;i++) {
 			
 		  if(n % i ==0) {
-			System.out.println(i +"");
+			System.out.print(i +" ");
 		}
 		
 	}
-	System.out.println(n);
+//	System.out.println(n);
 	}
 
 
